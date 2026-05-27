08c766d3a8cdb491a606c88702feb27937c3d131
reitaghchi <ren.len2426@gmail.com>
2026-05-27 09:10:19 +0900

    [update]プレゼンター作成

--- Assets/Code/Scripts/Presentation/Player/MobilePlayerPresenter.cs
+++ Assets/Code/Scripts/Presentation/Player/MobilePlayerPresenter.cs
@@ -1,9 +1,33 @@
+using Kizami.Application.Runtime;
+using Kizami.Presentation.Runtime.Player;
 using UnityEngine;
 
 namespace Kizami.Presentation.Runtime
 {
-    public class MobilePlayerPresenter
+    public class MobilePlayerPresenter : IMobilePlayerPresenter
     {
-    
+        public Vector3 Velocity
+        {
+            get => _mobilePlayerView.Velocity;
+            set => _mobilePlayerView.Velocity = value;
+        }
+
+        public Quaternion Rotation
+        {
+            get => _mobilePlayerView.Rotation;
+            set => _mobilePlayerView.Rotation = value;
+        }
+
+        private IMobilePlayerView _mobilePlayerView;
+
+        public MobilePlayerPresenter(IMobilePlayerView mobilePlayerView)
+        {
+            _mobilePlayerView = mobilePlayerView;
+        }
+
+        public void AddForce(Vector3 force, ForceMode mode)
+        {
+            _mobilePlayerView.AddForce(force, mode);
+        }
     }
 }
