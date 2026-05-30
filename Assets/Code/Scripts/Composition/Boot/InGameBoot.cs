f5505086774c94c9c0d88531a17931a1ffff2347
reitaghchi <ren.len2426@gmail.com>
2026-05-30 15:43:45 +0900

    [update]初期化を再生成

--- Assets/Code/Scripts/Composition/Boot/InGameBoot.cs
+++ Assets/Code/Scripts/Composition/Boot/InGameBoot.cs
@@ -1,5 +1,7 @@
 using Kizami.Application.Runtime;
+using Kizami.Application.Runtime.Player;
 using Kizami.Composition.Runtime;
+using Kizami.Composition.Runtime.Enemy;
 using Kizami.Infrastructure.Runtime;
 using MeshBreak.MeshCut.Version2;
 using UnityEngine;
@@ -14,6 +16,7 @@ namespace UsefulTools.Composition.Runtime.Boot
 
         [SerializeField] private InputInitializer _inputInitializer;
         [SerializeField] private UiInitializer _uiInitializer;
+        [SerializeField] private EnemyInitializer _enemyInitializer;
         [SerializeField] private PlayerInitializer _playerInitializer;
         [SerializeField] private BladeInitializer _bladeInitializer;
         [SerializeField] private MeshDataCache _meshDataCache;
@@ -26,6 +29,10 @@ namespace UsefulTools.Composition.Runtime.Boot
 
         private void Inject()
         {
+            if (_enemyInitializer != null && _container.TryGet<IPlayerDataGateway>(out var argenemyInitializer_0))
+            {
+                _enemyInitializer.Inject(argenemyInitializer_0);
+            }
             if (_playerInitializer != null && _container.TryGet<IInputDispatcher>(out var argplayerInitializer_0) && _container.TryGet<IBladePresenter>(out var argplayerInitializer_1))
             {
                 _playerInitializer.Inject(argplayerInitializer_0, argplayerInitializer_1);
@@ -36,6 +43,7 @@ namespace UsefulTools.Composition.Runtime.Boot
         {
             if (_inputInitializer != null) _inputInitializer.Initialize();
             if (_uiInitializer != null) _uiInitializer.Initialize();
+            if (_enemyInitializer != null) _enemyInitializer.Initialize();
             if (_playerInitializer != null) _playerInitializer.Initialize();
             if (_bladeInitializer != null) _bladeInitializer.Initialize();
             if (_meshDataCache != null) _meshDataCache.Initialize();
