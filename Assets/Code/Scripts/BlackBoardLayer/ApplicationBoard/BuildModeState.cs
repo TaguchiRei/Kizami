cc897dee3ebcdd64a530919ce05ae8c6f0b3a57c
reitaghchi <ren.len2426@gmail.com>
2026-08-22 17:12:31 +0900

    [add] ビルドモードをステート化

---
+++ Assets/Code/Scripts/BlackBoardLayer/ApplicationBoard/BuildModeState.cs
@@ -0,0 +1,34 @@
+using UsefulToolkit.BlackBoard.BlackBoard;
+
+namespace Kizami.BlackBoard
+{
+    /// <summary>
+    /// ビルドモードを保持するゲームステート。
+    /// </summary>
+    public class BuildModeState : GameStateBase
+    {
+        public BuildMode BuildMode { get; private set; }
+
+        public void SetBuildMode(BuildMode mode)
+        {
+            BuildMode = mode;
+        }
+
+        public override string GetLog()
+        {
+            return BuildMode.ToString();
+        }
+    }
+
+    public interface IBuildModeState : IStateGetter
+    {
+        BuildMode BuildMode { get; }
+    }
+
+    public enum BuildMode
+    {
+        PC,
+        Mobile,
+        VR
+    }
+}
